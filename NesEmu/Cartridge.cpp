1b0f3aa851d4001e51d22ae36d0f88a7bdb7f223
barry <barry.p.evans@gmail.com>
2021-01-22 12:21:50 -0800

    Isolate mapper to mapping only. store prg and chr mem in cartridge

--- NesEmu/Cartridge.cpp
+++ NesEmu/Cartridge.cpp
@@ -3,16 +3,57 @@
 Cartridge::Cartridge(std::string romPath, Bus * m_pCpuBus, Bus * m_pPpuBus)
 {
 	m_cartridgeReady = false;
+
 	//TODO: Read iNes header
 	//TODO: Initialize mapper
 
-	m_pCpuBus->RegisterMemoryDevice(m_pMapper->m_pCpuInterface, 0x4020);
-	m_pPpuBus->RegisterMemoryDevice(m_pMapper->m_pPpuInterface, 0x0000);
-
+	m_pCpuBus->RegisterMemoryDevice(m_pCpuInterface, 0x4020);
+	m_pPpuBus->RegisterMemoryDevice(m_pPpuInterface, 0x0000);
+	m_cartridgeReady = true;
 }
 
 Cartridge::~Cartridge()
 {
 	if (m_pMapper)
 		delete(m_pMapper);
+}
+
+uint8_t Cartridge::CpuRead(uint16_t addr)
+{
+	uint16_t finalAddr;
+	if(m_pMapper->CpuRemap(addr, finalAddr))
+		return m_pPrgMemory[finalAddr];
+
+	return 0x00;
+}
+
+bool Cartridge::CpuWrite(uint16_t addr, uint8_t data)
+{
+	uint16_t finalAddr;
+	if (m_pMapper->CpuRemap(addr, finalAddr))
+	{
+		m_pPrgMemory[finalAddr] = data;
+		return true;
+	}
+	return false;
+}
+
+uint8_t Cartridge::PpuRead(uint16_t addr)
+{
+	uint16_t finalAddr;
+	if (m_pMapper->PpuRemap(addr, finalAddr))
+		return m_pChrMemory[finalAddr];
+
+	return 0x00;
+}
+
+bool Cartridge::PpuWrite(uint16_t addr, uint8_t data)
+{
+	uint16_t finalAddr;
+	if (m_pMapper->PpuRemap(addr, finalAddr))
+	{
+		m_pChrMemory[finalAddr] = data;
+		return true;
+	}
+	return false;
 }
--- NesEmu/Cartridge.h
+++ NesEmu/Cartridge.h
@@ -12,11 +12,45 @@ public:
 	Cartridge(std::string romPath, Bus* m_pCpuBus, Bus* m_pPpuBus);
 	~Cartridge();
 	
+	uint8_t CpuRead(uint16_t addr);
+	bool CpuWrite(uint16_t addr, uint8_t data);
+	uint8_t PpuRead(uint16_t addr);
+	bool PpuWrite(uint16_t addr, uint8_t data);
+
 	bool IsCatridgeReady() { return m_cartridgeReady; }
 private:
+
+	uint8_t numPrgBanks = 0;
+	uint8_t numChrBanks = 0;
+	uint8_t* m_pPrgMemory;
+	uint8_t* m_pChrMemory;
 
 	bool m_cartridgeReady = false;
 	IMapper* m_pMapper;
 	INesHeader m_iNesHeader;
+
+	CartridgePPUInterface* m_pPpuInterface;
+	CartridgeCPUInterface* m_pCpuInterface;
 };
 
+class CartridgePPUInterface : public IMemoryDevice
+{
+public:
+	Cartridge* m_pCart;
+	CartridgePPUInterface(Cartridge* pCart) { m_pCart = pCart; }
+	virtual uint8_t Read(uint16_t addr) override { return m_pCart->PpuRead(addr); }
+	virtual bool Write(uint16_t addr, uint8_t data) override { return m_pCart->PpuWrite(addr, data); }
+	virtual uint16_t GetSize() override { return 0x2000; /*0x0000 -> 0x2000*/ }
+	virtual bool UseVirtualAddressSpace() override { return false; }
+};
+
+class CartridgeCPUInterface : public IMemoryDevice
+{
+public:
+	Cartridge* m_pCart;
+	CartridgePPUInterface(Cartridge* pCart) { m_pCart = pCart; }
+	virtual uint8_t Read(uint16_t addr) override { return m_pCart->CpuRead(addr); }
+	virtual bool Write(uint16_t addr, uint8_t data) override { return m_pCart->CpuWrite(addr, data); }
+	virtual uint16_t GetSize() override { return 0xBFDF; /*0x4020 -> 0xFFFF*/ }
+	virtual bool UseVirtualAddressSpace() override { return false; }
+};
